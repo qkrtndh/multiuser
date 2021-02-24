ed16d648ced0b9a8e5b6c1ba8e4a4c44f148127b
qkrtndh <qkrtndh5@naver.com>
2021-02-24 20:40:28 +0900

    회원가입 ui추가 및 입력 정보 저장 및 유효성 체크

--- routes/auth.js
+++ routes/auth.js
@@ -4,8 +4,14 @@ var path = require('path');
 var fs = require('fs');
 var sanitizeHtml = require('sanitize-html');
 var template = require('../lib/template.js');
+var shortid = require('shortid');
 
+const low = require('lowdb')
+const FileSync = require('lowdb/adapters/FileSync')
 
+const adapter = new FileSync('db.json')
+const db = low(adapter)
+db.defaults({ users: [] }).write()
 
 
 module.exports = function (passport) {
@@ -37,6 +43,53 @@ module.exports = function (passport) {
       failureFlash: true,
       successFlash: true
     }));
+
+  router.get('/register', function (request, response) {
+    var fmsg = request.flash();
+    var feedback = '';
+    if (fmsg.error) {
+      feedback = fmsg.error[0];
+    }
+    var title = 'WEB - register';
+    var list = template.list(request.list);
+    var html = template.HTML(title, list, `
+        <div style="color:red;">${feedback}</div>
+        <form action="/auth/register_process" method="post">
+          <p><input type="text" name="email" placeholder="email"></p>
+          <p><input type="password" name="pwd" placeholder="password"></p>
+          <p><input type="password" name="pwd2" placeholder="password"></p>
+          <p><input type="text" name="nickname" placeholder="nickname"></p>
+          <p>
+            <input type="submit" value="register">
+          </p>
+        </form>
+      `, '');
+    response.send(html);
+  });
+
+  router.post('/register_process', function (request, response) {//정보를 받아온다.
+    var post = request.body;
+    var email = post.email;
+    var pwd = post.pwd;
+    var pwd2 = post.pwd2;
+    var nickname = post.nickname;
+    if (db.get('users').find({ email: email }).value()) {
+      request.flash('error', 'email already exist');
+      response.redirect('/auth/register');
+    }
+
+    else {
+      if (pwd != pwd2) {
+        request.flash('error', 'password not match');
+        response.redirect('/auth/register');
+      }
+      else {
+        db.get('users').push({ id: shortid.generate(), email: email, password: pwd, nickname: nickname }).write();
+        response.redirect('/auth/login');
+      }
+    }
+  });
+
 
   router.get('/logout', function (request, response) {
     request.logout();
